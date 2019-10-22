2862ff5706c272e6a0dc7d996325787b454f05e0
RaumfahrerSpiffy <56406949+RaumfahrerSpiffy@users.noreply.github.com>
2019-10-22 18:39:37 -0400

    Update hidpi.sh

--- hidpi.sh
+++ hidpi.sh
@@ -33,7 +33,7 @@ langChooseResOp2="(2) 1080P Display (use 1424x802, fix underscaled after sleep)"
 langChooseResOp3="(3) 2K Display"
 langChooseResOp4="(4) Manual input resolution"
 
-downloadHost="https://raw.githubusercontent.com/xzhih/one-key-hidpi/master"
+#downloadHost="https://raw.githubusercontent.com/xzhih/one-key-hidpi/master"
 # downloadHost="https://raw.githubusercontent.com/xzhih/one-key-hidpi/dev"
 # downloadHost="http://127.0.0.1:8080"
 
@@ -147,7 +147,7 @@ function get_edid()
 function init()
 {
 #
-cat << EEF
+EEF
   _    _   _____   _____    _____    _____ 
  | |  | | |_   _| |  __ \  |  __ \  |_   _|
  | |__| |   | |   | |  | | | |__) |   | |  
